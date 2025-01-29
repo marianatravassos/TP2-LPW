9c07cab1650e1f39550bfb5d9b7178e64bacb957
Guilherme Malard <guilhermemfpereira@gmail.com>
2025-01-28 23:17:18 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,15 +1,1 @@
-const loginForm = document.getElementById('loginForm');
-    loginForm.addEventListener('submit', function(event) {
-        event.preventDefault();
 
-        const username = loginForm.username.value;
-        const password = loginForm.password.value;
-
-        const storedUser = localStorage.getItem(username);
-
-        if (storedUser && storedUser === password) {
-            window.location.href = "inicio.html";
-        } else {
-            alert('Usuário ou senha incorretos!');
-        }
-    });
