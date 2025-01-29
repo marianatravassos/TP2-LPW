d637c017a53cecdc95d2fa307efe80684ca4418a
Guilherme Malard <guilhermemfpereira@gmail.com>
2025-01-28 22:49:18 -0300

    Fiz os créditos.

---
+++ cheat.js
@@ -0,0 +1,16 @@
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.addEventListener('click', function(){
+    modal.style.display = "block";
+});
+span.addEventListener('click', function(){
+    modal.style.display = "none";
+})
+window.addEventListener('click', function(event){
+    if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}) 
+
