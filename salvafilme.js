4c115b3f59022402329e36df9e2d6ec9262f055a
Guilherme Malard <guilhermemfpereira@gmail.com>
2025-01-28 23:16:33 -0300

    Update salvafilme.js

--- salvafilme.js
+++ salvafilme.js
@@ -1,19 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const addButton = document.querySelector(".botaoAdicionar");
 
-    if (addButton) {
-        addButton.addEventListener("click", () => {
-            const movieTitle = document.querySelector("h1").textContent;
-
-            const watchedMovies = JSON.parse(localStorage.getItem("watchedMovies")) || [];
-
-            if (!watchedMovies.includes(movieTitle)) {
-                watchedMovies.push(movieTitle);
-                localStorage.setItem("watchedMovies", JSON.stringify(watchedMovies));
-                alert(`${movieTitle} foi adicionado à lista!`);
-            } else {
-                alert(`${movieTitle} já está na sua lista.`);
-            }
-        });
-    }
-});
